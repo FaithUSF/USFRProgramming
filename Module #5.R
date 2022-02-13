634003680c0048952faa7b383a17feaf9879ec3e
FaithUSF <97637960+FaithUSF@users.noreply.github.com>
2022-02-13 13:43:11 -0500

    Add files via upload

---
+++ Module #5.R
@@ -0,0 +1,14 @@
+A <-matrix(sample(1:100), nrow = 10)
+B <- matrix(sample(1:1000), nrow = 10)
+t(A)
+t(B)
+a <- c(1:2)
+b <- c(1:4)
+X <- a*A
+Y <- b*B
+a <- c(1:10)
+b <- c(1:100)
+A%*%a
+B%*%b
+S <- matrix(2:5, nrow = 2)
+det(S)
